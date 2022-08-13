b95a2329ec9c2688340eadc93f92e5cb0f87da53
loxloxloxlox <senja2018@mail.ru>
2022-08-13 20:53:36 +0300

    test

---
+++ src/main.rs
@@ -0,0 +1,28 @@
+use std::io::prelude::*;
+use std::net::TcpStream;
+use std::net::TcpListener;
+
+fn main() {
+    let listener = TcpListener::bind("127.0.0.1:3333").unwrap();
+
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+        
+        handle_connection(stream);
+    }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let mut buffer = [0; 512];
+
+    stream.read(&mut buffer).unwrap();
+
+    let str = String::from_utf8_lossy(&buffer[..]);
+
+    if str.chars().nth(0).unwrap() == '9' {
+        println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
+    }
+    else {
+        println!("err");
+    }
+}
